77d63394630302c8a86cdddb9f1e9d356e3a1f53
(no author) <(no author)@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2001-02-08 18:44:30 +0000

    This commit was manufactured by cvs2svn to create tag 'vnc-tight-1-1p8'.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@861 3789f03b-4d11-0410-bbf8-ca57d06f2519

---
+++ orig/tags/vnc-tight-1-1p8/vnc_winsrc/winvnc/vncBuffer.h
@@ -0,0 +1,125 @@
+//  Copyright (C) 2000 Tridia Corporation. All Rights Reserved.
+//  Copyright (C) 1999 AT&T Laboratories Cambridge. All Rights Reserved.
+//
+//  This file is part of the VNC system.
+//
+//  The VNC system is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+//  USA.
+//
+// For the latest source code, please check:
+//
+// http://www.DevelopVNC.org/
+//
+// or send email to: feedback@developvnc.org.
+//
+// If the source code for the VNC system is not available from the place 
+// whence you received this file, check http://www.uk.research.att.com/vnc or contact
+// the authors on vnc@uk.research.att.com for information on obtaining it.
+
+// vncBuffer object
+
+// The vncBuffer object provides a client-local copy of the screen
+// It can tell the client which bits have changed in a given region
+// It uses the specified vncDesktop to read screen data from
+
+class vncBuffer;
+
+#if !defined(_WINVNC_VNCBUFFER)
+#define _WINVNC_VNCBUFFER
+#pragma once
+
+// Includes
+
+#include "vncDesktop.h"
+#include "vncEncoder.h"
+#include "vncRegion.h"
+#include "RectList.h"
+
+// Class definition
+
+class vncBuffer
+{
+// Methods
+public:
+	// Create/Destroy methods
+	vncBuffer(vncDesktop *desktop);
+	~vncBuffer();
+
+	// BUFFER INFO
+	RECT GetSize();
+	rfbPixelFormat GetLocalFormat();
+	BYTE *GetClientBuffer();
+	UINT GetClientBuffSize();
+	BOOL GetRemotePalette(RGBQUAD *quadbuff, UINT ncolours);
+
+	// BUFFER MANIPULATION
+	BOOL CheckBuffer();
+
+	// SCREEN SCANNING
+	void Clear(RECT &rect);
+	void GetChangedRegion(vncRegion &rgn, RECT &rect);											
+	UINT GetNumCodedRects(RECT &rect);
+
+	// SCREEN CAPTURE
+	void GrabRect(RECT &rect);
+	void CopyRect(RECT &dest, POINT &source);
+	RECT GrabMouse();
+	BOOL SetClientFormat(rfbPixelFormat &format);
+
+	// ENCODING
+	BOOL SetEncoding(CARD32 encoding);
+	UINT TranslateRect(const RECT &rect, VSocket *outConn);
+	BOOL SetCompressLevel(CARD32 level);
+	BOOL SetQualityLevel(CARD32 level);
+	BOOL EnableLastRect(BOOL enable);
+	BOOL IsLastRectEnabled() { return m_use_lastrect; }
+
+	HCURSOR GetCursor();
+
+// Implementation
+protected:
+
+	// Routine to verify the mainbuff handle hasn't changed
+	inline BOOL FastCheckMainbuffer();
+	
+	BYTE		*m_mainbuff;
+	BOOL		m_freemainbuff;
+	BYTE		*m_backbuff;
+	UINT		m_mainsize;
+
+	BYTE		*m_clientbuff;
+	UINT		m_clientbuffsize;
+	BOOL		m_clientfmtset;
+
+	UINT		m_bytesPerRow;
+
+	rfbServerInitMsg	m_scrinfo;
+	rfbPixelFormat		m_clientformat;
+	rfbTranslateFnType	m_transfunc;
+
+	vncDesktop	   *m_desktop;
+	vncEncoder	   *m_encoder;
+	bool           zlib_encoder_in_use;
+	vncEncoder     *m_hold_zlib_encoder;
+	bool           tight_encoder_in_use;
+	vncEncoder     *m_hold_tight_encoder;
+	bool           zlibhex_encoder_in_use;
+	vncEncoder     *m_hold_zlibhex_encoder;
+	UINT			m_compresslevel;
+	UINT			m_qualitylevel;
+	BOOL            m_use_lastrect;
+};
+
+#endif // _WINVNC_VNCBUFFER

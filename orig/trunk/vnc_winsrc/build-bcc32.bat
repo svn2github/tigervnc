7e6976466bb09fd4e52fba1d555e28b4c755bfa1
const <const@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2001-03-13 12:44:16 +0000

    More porting to Borland C++ 5.5 compiler. Now build scripts and makefiles should work in Win95/98 environment.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@896 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- orig/trunk/vnc_winsrc/build-bcc32.bat
+++ orig/trunk/vnc_winsrc/build-bcc32.bat
@@ -1,12 +1,11 @@
 @echo off
-
-mkdir binaries\
-cd vncviewer\
+mkdir binaries
+cd vncviewer
 make -fMakefile.bcc32 -s
 cd ..
-copy /b vncviewer\vncviewer.exe binaries\
-cd winvnc\
+copy /b vncviewer\vncviewer.exe binaries
+cd winvnc
 make -fMakefile.bcc32 -s
 cd ..
-copy /b winvnc\VNCHooks\VNCHooks.dll binaries\
-copy /b winvnc\winvnc.exe binaries\
+copy /b winvnc\VNCHooks\VNCHooks.dll binaries
+copy /b winvnc\winvnc.exe binaries

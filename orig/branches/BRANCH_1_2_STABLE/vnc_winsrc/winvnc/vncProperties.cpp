e9577f1a11b67b7b2499ecaa68a30f4eb5e1ebd4
const_k <const_k@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2003-07-10 22:33:04 +0000

    Restored the code closing the HKCU registry hive. This code was removed to workaround a password reset problem, but had introduced another issue that can be considered even worse. Now we should find other ways to fix old password reset problem.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@1509 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- orig/branches/BRANCH_1_2_STABLE/vnc_winsrc/winvnc/vncProperties.cpp
+++ orig/branches/BRANCH_1_2_STABLE/vnc_winsrc/winvnc/vncProperties.cpp
@@ -934,6 +934,9 @@ vncProperties::Load(BOOL usersettings)
 				vnclog.Print(LL_INTINFO, VNCLOG("loading \"%s\" global settings\n"), username);
 				LoadUserPrefs(hkGlobalUser);
 				RegCloseKey(hkGlobalUser);
+
+				// Close the user registry hive so it can unload if required
+				RegCloseKey(HKEY_CURRENT_USER);
 			}
 		}
 	} else {

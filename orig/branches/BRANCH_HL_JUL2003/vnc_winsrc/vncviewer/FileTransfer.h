2c89f27b3736dddd02f4ce1b8a7002ed4a392c2d
(no author) <(no author)@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2003-07-15 13:35:09 +0000

    This commit was manufactured by cvs2svn to create branch 'BRANCH_HL_JUL2003'.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@1514 3789f03b-4d11-0410-bbf8-ca57d06f2519

---
+++ orig/branches/BRANCH_HL_JUL2003/vnc_winsrc/vncviewer/FileTransfer.h
@@ -0,0 +1,87 @@
+// FileTransfer.h: interface for the FileTransfer class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(FILETRANSFER)
+#define FILETRANSFER
+
+#define rfbMAX_PATH 255
+
+#include "windows.h"
+#include "commctrl.h"
+#include "ClientConnection.h"
+
+class ClientConnection;
+
+typedef struct tagFTITEMINFO
+{
+    char Name[rfbMAX_PATH];
+    char Size[16];
+} FTITEMINFO;
+
+class FileTransfer  
+{
+private:
+	static const char uploadText[];
+	static const char downloadText[];
+	static const char noactionText[];
+
+public:
+	static const char folderText[];
+
+	FileTransfer(ClientConnection * pCC, VNCviewerApp * pApp);
+	~FileTransfer();
+
+	void FTInsertColumn(HWND hwnd, char *iText, int iOrder, int xWidth);
+	int GetLogicalDriveStringByType(unsigned int DriveType, char *DrivesString);
+	void CreateFileTransferDialog();
+	void ShowListViewItems(HWND hwnd, FTITEMINFO *FTItemInfo, int NumItem);
+	void ConvertPath(char *path);
+	void ProcessListViewDBLCLK(HWND hwnd, char *Path, char *PathTmp, int iItem);
+	void SendFileListRequestMessage(char *filename);
+	void ShowServerItems();
+	void ShowClientItems(char *path);
+	void BlockingFileTransferDialog(BOOL status);
+	void ProcessDlgMessage(HWND hwnd);
+	void ShowTreeViewItems(HWND hwnd, LPNMTREEVIEW m_lParam);
+	void CreateFTBrowseDialog(BOOL status);
+	void StrInvert(char *str);
+	void GetTVPath(HWND hwnd, HTREEITEM hTItem, char *path);
+	char m_ClientPath[rfbMAX_PATH];
+	char m_ServerPath[rfbMAX_PATH];
+	char m_ServerPathTmp[rfbMAX_PATH];
+	char m_ClientPathTmp[rfbMAX_PATH];
+	char m_ClientFilename[rfbMAX_PATH];
+	char m_ServerFilename[rfbMAX_PATH];
+	FTITEMINFO * m_FTClientItemInfo;
+	FTITEMINFO * m_FTServerItemInfo;
+	void OnGetDispClientInfo(NMLVDISPINFO *plvdi); 
+	void OnGetDispServerInfo(NMLVDISPINFO *plvdi); 
+	static LRESULT CALLBACK FileTransferDlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+	static BOOL CALLBACK FTBrowseDlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+	void FileTransferDownload();
+	void FileTransferUpload();
+
+	ClientConnection * m_clientconn;
+	VNCviewerApp * m_pApp; 
+	
+private:
+	HWND m_hwndFileTransfer;
+	HWND m_hwndFTClientList;
+	HWND m_hwndFTServerList;
+	HWND m_hwndFTClientPath;
+	HWND m_hwndFTServerPath;
+	HWND m_hwndFTProgress;
+	HWND m_hwndFTStatus;
+	HWND m_hwndFTBrowse;
+	
+	BOOL m_TransferEnable;
+	BOOL m_bServerBrowseRequest;
+	BOOL m_bFTCOPY;
+
+	HANDLE m_hFiletoWrite;
+	HTREEITEM m_hTreeItem;
+	HINSTANCE m_FTInstance;
+};
+
+#endif // !defined(FILETRANSFER)

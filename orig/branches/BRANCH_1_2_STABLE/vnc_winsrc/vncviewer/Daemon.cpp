35075f142aaeb344ec2fe51f742de5dd8e111f54
const_k <const_k@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2003-07-31 11:30:37 +0000

    Minor fix to prevent crashing on a socket error, in the listening mode.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@1542 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- orig/branches/BRANCH_1_2_STABLE/vnc_winsrc/vncviewer/Daemon.cpp
+++ orig/branches/BRANCH_1_2_STABLE/vnc_winsrc/vncviewer/Daemon.cpp
@@ -169,7 +169,10 @@ LRESULT CALLBACK Daemon::WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lPa
 
 	case WM_SOCKEVENT:
 		{
-			assert(HIWORD(lParam) == 0);
+			// Ignore errors.
+			if (HIWORD(lParam) != 0) {
+				return 0;
+			}
 			// A new socket created by accept might send messages to
 			// this procedure. We can ignore them.
 			if(wParam != _this->m_sock) {

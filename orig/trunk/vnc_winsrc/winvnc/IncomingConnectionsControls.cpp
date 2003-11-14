fdf23a11d10c62bd705e6d34ff1db5eb5aa90c08
oleg_sh <oleg_sh@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2003-11-14 20:40:31 +0000

    In tab "Incoming Connections" of the "Properties" dialog box the setting of a focus  of input in fields of input "Primary password", "display" and "main port" is made when the switches "Accept socket connections",  "display" and "ports"  are checked accordingly .
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@1589 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- orig/trunk/vnc_winsrc/winvnc/IncomingConnectionsControls.cpp
+++ orig/trunk/vnc_winsrc/winvnc/IncomingConnectionsControls.cpp
@@ -10,42 +10,55 @@
 
 IncomingConnectionsControls::IncomingConnectionsControls(HWND hwnd, vncServer *server)
 {
-  m_server = server;
-  m_hwnd = hwnd;
-  Init();
+	m_server = server;
+	m_hwnd = hwnd;
+	Init();
 }
 
 void IncomingConnectionsControls::Validate()
 {
-  BOOL bAccept = IsChecked(IDC_CONNECT_SOCK);
-  BOOL bAuto = IsChecked(IDC_PORTNO_AUTO);
-  BOOL bDisplay = IsChecked(IDC_SPECDISPLAY);
-  BOOL bPorts = IsChecked(IDC_SPECPORT);
-  Enable(IDC_PASSWORD_LABEL, bAccept);
-  Enable(IDC_PASSWORD_VIEWONLY_LABEL, bAccept);
-  Enable(IDC_PASSWORD, bAccept);
-  Enable(IDC_PASSWORD_VIEWONLY, bAccept);
-  Enable(IDC_PORTNO_AUTO, bAccept);
-  Enable(IDC_SPECDISPLAY, bAccept);
-  Enable(IDC_SPECPORT, bAccept);
+	BOOL bAccept = IsChecked(IDC_CONNECT_SOCK);
+	BOOL bAuto = IsChecked(IDC_PORTNO_AUTO);
+	BOOL bDisplay = IsChecked(IDC_SPECDISPLAY);
+	BOOL bPorts = IsChecked(IDC_SPECPORT);
+	Enable(IDC_PASSWORD_LABEL, bAccept);
+	Enable(IDC_PASSWORD_VIEWONLY_LABEL, bAccept);
+	Enable(IDC_PASSWORD, bAccept);
+	Enable(IDC_PASSWORD_VIEWONLY, bAccept);
+	Enable(IDC_PORTNO_AUTO, bAccept);
+	Enable(IDC_SPECDISPLAY, bAccept);
+	Enable(IDC_SPECPORT, bAccept);
+	
+	Enable(IDC_SPECPORT, bAccept);
+	Enable(IDC_PORTNO_AUTO, bAccept);
+	Enable(IDC_SPECDISPLAY, bAccept);
+	
+	Enable(IDC_DISPLAYNO, bAccept && bDisplay);
+	
+	Enable(IDC_PORTRFB, bAccept && bPorts);
+	Enable(IDC_MAIN_LABEL, bAccept && bPorts);
+	Enable(IDC_AND_LABEL, bAccept && bPorts);
+	Enable(IDC_PORTHTTP, bAccept && bPorts);
+	Enable(IDC_HTTP_LABEL, bAccept && bPorts);
+	
+	HWND hFocus = GetFocus();
+	if ((hFocus == GetDlgItem(m_hwnd, IDC_CONNECT_SOCK)) ||
+		(hFocus == m_hwnd))	
+		SetFocus(GetDlgItem(m_hwnd, IDC_PASSWORD));
 
-  Enable(IDC_SPECPORT, bAccept);
-  Enable(IDC_PORTNO_AUTO, bAccept);
-  Enable(IDC_SPECDISPLAY, bAccept);
+	if (hFocus == GetDlgItem(m_hwnd, IDC_SPECDISPLAY)) {
+		SetFocus(GetDlgItem(m_hwnd, IDC_DISPLAYNO));
+		SendMessage(GetDlgItem(m_hwnd, IDC_DISPLAYNO), EM_SETSEL, 0, (LPARAM)-1);
+	}
 
-  Enable(IDC_DISPLAYNO, bAccept && bDisplay);
-
-  Enable(IDC_PORTRFB, bAccept && bPorts);
-  Enable(IDC_MAIN_LABEL, bAccept && bPorts);
-  Enable(IDC_AND_LABEL, bAccept && bPorts);
-  Enable(IDC_PORTHTTP, bAccept && bPorts);
-  Enable(IDC_HTTP_LABEL, bAccept && bPorts);
-
-
+	if (hFocus == GetDlgItem(m_hwnd, IDC_SPECPORT)) {
+		SetFocus(GetDlgItem(m_hwnd, IDC_PORTRFB));
+		SendMessage(GetDlgItem(m_hwnd, IDC_PORTRFB), EM_SETSEL, 0, (LPARAM)-1);
+	}
 }
 void IncomingConnectionsControls::Apply()
 {
-  // Save the password if one was entered
+	// Save the password if one was entered
 	char passwd[MAXPWLEN+1];
 	int len = GetDlgItemText(m_hwnd, IDC_PASSWORD, (LPSTR)&passwd, MAXPWLEN+1);
 	if (strcmp(passwd, "~~~~~~~~") != 0) {
@@ -57,7 +70,7 @@ void IncomingConnectionsControls::Apply()
 			m_server->SetPassword(crypt);
 		}
 	}
-
+	
 	// Save the password (view only) if one was entered
 	// FIXME: Code duplication, see above
 	len = GetDlgItemText(m_hwnd, IDC_PASSWORD_VIEWONLY, (LPSTR)&passwd, MAXPWLEN+1);
@@ -70,10 +83,10 @@ void IncomingConnectionsControls::Apply()
 			m_server->SetPasswordViewOnly(crypt);
 		}
 	}
-
+	
 	// Save the new settings to the server
 	m_server->SetAutoPortSelect(IsChecked(IDC_PORTNO_AUTO));
-
+	
 	// Save port numbers if we're not auto selecting
 	if (!m_server->AutoPortSelect()) {
 		if (IsChecked(IDC_SPECDISPLAY)) {
@@ -82,7 +95,7 @@ void IncomingConnectionsControls::Apply()
 			UINT display = GetDlgItemInt(m_hwnd, IDC_DISPLAYNO, &ok, TRUE);
 			if (ok)
 				m_server->SetPorts(DISPLAY_TO_PORT(display),
-													  DISPLAY_TO_HPORT(display));
+				DISPLAY_TO_HPORT(display));
 		} else {
 			// Assuming that port numbers were specified
 			BOOL ok1, ok2;
@@ -92,40 +105,40 @@ void IncomingConnectionsControls::Apply()
 				m_server->SetPorts(port_rfb, port_http);
 		}
 	}
-
+	
 	m_server->SockConnect(IsChecked(IDC_CONNECT_SOCK));
-		
-  // Wallpaper handling
+	
+	// Wallpaper handling
 	m_server->EnableRemoveWallpaper(IsChecked(IDC_REMOVE_WALLPAPER));
-		
+	
 	// Enabling/disabling file transfers
 	m_server->EnableFileTransfers(IsChecked(IDC_ENABLE_FILE_TRANSFERS));
 }
 
 void IncomingConnectionsControls::Init()
 {
-  BOOL bConnectSock = m_server->SockConnected();
+	BOOL bConnectSock = m_server->SockConnected();
 	BOOL bAutoPort = m_server->AutoPortSelect();
 	UINT port_rfb = m_server->GetPort();
 	UINT port_http = m_server->GetHttpPort();
 	int d1 = PORT_TO_DISPLAY(port_rfb);
 	int d2 = HPORT_TO_DISPLAY(port_http);
 	BOOL bValidDisplay = (d1 == d2 && d1 >= 0 && d1 <= 99);
-
-  if (bConnectSock) {
+	
+	if (bConnectSock) {
 		SetFocus(GetDlgItem(m_hwnd, IDC_PASSWORD));
 		SendDlgItemMessage(m_hwnd, IDC_PASSWORD, EM_SETSEL, 0, (LPARAM)-1);
 	} else {
 		SetFocus(GetDlgItem(m_hwnd, IDC_CONNECT_SOCK));
 	}
-  
-  SetChecked(IDC_CONNECT_SOCK, bConnectSock);
+	
+	SetChecked(IDC_CONNECT_SOCK, bConnectSock);
 	SetChecked(IDC_PORTNO_AUTO, bAutoPort);
 	SetChecked(IDC_SPECDISPLAY, (!bAutoPort && bValidDisplay));
 	SetChecked(IDC_SPECPORT, (!bAutoPort && !bValidDisplay));
-  SetChecked(IDC_ENABLE_FILE_TRANSFERS, m_server->FileTransfersEnabled());
-  SetChecked(IDC_REMOVE_WALLPAPER, m_server->RemoveWallpaperEnabled());
-
+	SetChecked(IDC_ENABLE_FILE_TRANSFERS, m_server->FileTransfersEnabled());
+	SetChecked(IDC_REMOVE_WALLPAPER, m_server->RemoveWallpaperEnabled());
+	
 	if (bValidDisplay) {
 		SetDlgItemInt(m_hwnd, IDC_DISPLAYNO, d1, FALSE);
 	} else {
@@ -133,12 +146,12 @@ void IncomingConnectionsControls::Init()
 	}
 	SetDlgItemInt(m_hwnd, IDC_PORTRFB, port_rfb, FALSE);
 	SetDlgItemInt(m_hwnd, IDC_PORTHTTP, port_http, FALSE);
-  SetDlgItemText(m_hwnd, IDC_PASSWORD, "~~~~~~~~");			
+	SetDlgItemText(m_hwnd, IDC_PASSWORD, "~~~~~~~~");			
 	SetDlgItemText(m_hwnd, IDC_PASSWORD_VIEWONLY, "~~~~~~~~");
-
-  Validate();
+	
+	Validate();
 }
 IncomingConnectionsControls::~IncomingConnectionsControls()
 {
-
+	
 }

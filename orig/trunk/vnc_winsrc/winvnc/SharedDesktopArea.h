e84498865aa67503096a165c4eeb4c63f7012c6e
const_k <const_k@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2004-07-29 10:05:16 +0000

    Added MatchWindow as an optional member pointer. Reduced duplicate code via changes to Init(). Normalized FullScreen(), SharedWindow() and SharedScreen(). A modified patch from HorizonLive.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@1794 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- orig/trunk/vnc_winsrc/winvnc/SharedDesktopArea.h
+++ orig/trunk/vnc_winsrc/winvnc/SharedDesktopArea.h
@@ -5,8 +5,13 @@
 #ifndef SHAREDDESKTOPAREA_H__
 #define SHAREDDESKTOPAREA_H__
 
+class SharedDesktopArea;
+
 #include "resource.h"
+
+#include "WinVNC.h"
 #include "vncServer.h"
+#include "vncProperties.h"
 #include "MatchWindow.h"
 
 class SharedDesktopArea  
@@ -19,11 +24,14 @@ public:
 	void SharedWindow();
 	void SharedScreen();
 	virtual ~SharedDesktopArea();
+
 protected:
 	void SetWindowCaption(HWND hWnd);
-	HWND hNameAppli;
+	HWND m_hWindowName;
+
 private:
 	void Init();
+	void SetupMatchWindow();
 	static void DrawFrameAroundWindow(HWND hWnd);
 	static LRESULT CALLBACK BmpWndProc(HWND, UINT, WPARAM, LPARAM);
 
@@ -31,9 +39,11 @@ private:
 	LONG m_OldBmpWndProc;
 	BOOL m_bCaptured;
 	HWND m_KeepHandle;
-	CMatchWindow *m_pMatchWindow;
 	vncServer *m_server;
 	vncProperties *m_vncprop;
+
+	CMatchWindow *m_pMatchWindow;
+	bool m_deleteMatchWindow;
 };
 
-#endif 
+#endif // SHAREDDESKTOPAREA_H__

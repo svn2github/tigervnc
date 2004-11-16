876ed73c04189cecc9e599d6501d36090bd9adc3
const_k <const_k@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2004-11-16 16:36:14 +0000

    From the head branch - added heading comments in files appeared recently.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@1951 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- orig/branches/BRANCH_1_3_STABLE/vnc_winsrc/winvnc/IncomingConnectionsControls.h
+++ orig/branches/BRANCH_1_3_STABLE/vnc_winsrc/winvnc/IncomingConnectionsControls.h
@@ -1,10 +1,26 @@
-// IncomingConnectionsControls.h: interface for the IncomingConnectionsControls class.
+// Copyright (C) 2004 TightVNC Development Team. All Rights Reserved.
 //
-//////////////////////////////////////////////////////////////////////
+//  TightVNC is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+//  USA.
+//
+// TightVNC homepage on the Web: http://www.tightvnc.com/
 
-#ifndef AFX_INCOMINGCONNECTIONSCONTROLS_H__
-#define AFX_INCOMINGCONNECTIONSCONTROLS_H__
+// IncomingConnectionsControls.h: interface for the IncomingConnectionsControls class.
 
+#ifndef _WINVNC_INCOMINGCONNECTIONSCONTROLS_H
+#define _WINVNC_INCOMINGCONNECTIONSCONTROLS_H
 #pragma once
 
 #include "resource.h"
@@ -35,4 +51,4 @@ private:
   HWND m_hwnd;
 };
 
-#endif 
+#endif

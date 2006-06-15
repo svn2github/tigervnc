9ada907aab204b91d835b995340400877157ad5f
const_k <const_k@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2006-06-15 03:34:45 +0000

    GUI improvements in the Hooks tab of the Properties dialog.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@2059 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- orig/branches/BRANCH_1_3_STABLE/vnc_winsrc/winvnc/resource.h
+++ orig/branches/BRANCH_1_3_STABLE/vnc_winsrc/winvnc/resource.h
@@ -137,6 +137,7 @@
 #define IDC_LIVESHARE_LABEL             1097
 #define IDC_DONT_USE_DRIVER             1098
 #define IDC_DRIVER_DIRECT_ACCESS        1099
+#define IDC_STATIC_DRVOPT               1100
 #define ID_PROPERTIES                   40001
 #define ID_CLOSE                        40002
 #define ID_KILLCLIENTS                  40003
@@ -151,7 +152,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        156
 #define _APS_NEXT_COMMAND_VALUE         40008
-#define _APS_NEXT_CONTROL_VALUE         1100
+#define _APS_NEXT_CONTROL_VALUE         1101
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif

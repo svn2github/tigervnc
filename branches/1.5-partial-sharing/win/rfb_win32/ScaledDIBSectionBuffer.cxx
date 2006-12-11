3e3dbf39eddcdbeba1efba3654ed8a6aaf898423
const_k <const_k@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2006-12-11 04:44:00 +0000

    New branch for working on partial screen sharing in WinVNC (e.g. share one area of the screen to one client and another area to another client).
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@2206 3789f03b-4d11-0410-bbf8-ca57d06f2519

---
+++ branches/1.5-partial-sharing/win/rfb_win32/ScaledDIBSectionBuffer.cxx
@@ -0,0 +1,180 @@
+/* Copyright (C) 2006 TightVNC Team.  All Rights Reserved.
+ *    
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+ * USA.
+ *
+ * TightVNC distribution homepage on the Web: http://www.tightvnc.com/
+ *
+ */
+
+// -=- ScaledDIBSectionBuffer.cxx
+
+#include <math.h>
+
+#include <rfb_win32/ScaledDIBSectionBuffer.h>
+
+using namespace rfb;
+using namespace win32;
+
+const PixelFormat RGB24(32, 24, 0, 1, 255, 255, 255, 16, 8, 0);
+
+ScaledDIBSectionBuffer::ScaledDIBSectionBuffer(HWND window) 
+  : src_buffer(0), scaling(false), DIBSectionBuffer(window) {
+  scaled_data = &data;
+}
+
+ScaledDIBSectionBuffer::~ScaledDIBSectionBuffer() {
+  if (src_buffer) delete src_buffer;
+}
+
+void ScaledDIBSectionBuffer::setScaleRatio(double scale_ratio_) {
+  if (scale_ratio == scale_ratio_ || scale_ratio <= 0) return;
+
+  scale_ratio = scale_ratio_;
+  if (scale_ratio == 1) scaling = false;
+  else scaling = true;
+  
+  // FIXME:
+  // Calculate the scale weight tabs must be in the ScalePixelBuffer class
+  freeWeightTabs();
+  calculateScaledBufferSize();
+  scaleFilters.makeWeightTabs(scaleFilterID, src_width, scaled_width, scale_ratio, &xWeightTabs);
+  scaleFilters.makeWeightTabs(scaleFilterID, src_height, scaled_height, scale_ratio, &yWeightTabs);
+
+  recreateBuffers();
+}
+
+void ScaledDIBSectionBuffer::setPF(const PixelFormat &pf_) {
+  if (memcmp(&(ScaledPixelBuffer::pf), &pf_, sizeof(pf_)) == 0) return;
+
+  if (!pf_.trueColour) throw rfb::UnsupportedPixelFormatException();
+
+  pf = pf_;
+  if (scaling) {
+    if (src_buffer) {
+      src_buffer->setPF(pf);
+    } else {
+      src_buffer = new ManagedPixelBuffer(pf, src_width, src_height);
+      src_data = &(src_buffer->data);
+    }
+    if (memcmp(&(DIBSectionBuffer::getPF()), &RGB24, sizeof(PixelFormat)) != 0) {
+      DIBSectionBuffer::setPF(RGB24);
+    }
+  } else {
+    DIBSectionBuffer::setPF(pf);
+  }
+}
+
+void ScaledDIBSectionBuffer::setSize(int src_width_, int src_height_) {
+  if (src_width == src_width_ && src_height == src_height_) return;
+  src_width = src_width_;
+  src_height = src_height_;
+  
+  // FIXME:
+  // Calculate the scale weight tabs must be in the ScalePixelBuffer class
+  freeWeightTabs();
+  calculateScaledBufferSize();
+  scaleFilters.makeWeightTabs(scaleFilterID, src_width, scaled_width, scale_ratio, &xWeightTabs);
+  scaleFilters.makeWeightTabs(scaleFilterID, src_height, scaled_height, scale_ratio, &yWeightTabs);
+
+  recreateBuffers();
+}
+
+void ScaledDIBSectionBuffer::recreateScaledBuffer() {
+  calculateScaledBufferSize();
+  if (scaling && memcmp(&(DIBSectionBuffer::getPF()), &RGB24, sizeof(PixelFormat)) != 0) {
+    DIBSectionBuffer::setPF(RGB24);
+  } else if (!scaling && (memcmp(&(DIBSectionBuffer::getPF()), &pf, sizeof(PixelFormat)) != 0)){
+    DIBSectionBuffer::setPF(pf);
+  } else {
+    DIBSectionBuffer::recreateBuffer();
+  }
+}
+
+void ScaledDIBSectionBuffer::recreateBuffers() {
+  // Recreate the source pixel buffer
+  if (src_width && src_height && pf.depth > 0) {
+    if (scaling) {
+      if (src_buffer) { 
+        if (src_buffer->width() != src_width || src_buffer->width() != src_height)
+          src_buffer->setSize(src_width, src_height);
+      } else {
+        src_buffer = new ManagedPixelBuffer(pf, src_width, src_height);
+        src_data = &(src_buffer->data);
+        if (data) memcpy(src_buffer->data, data, src_width * src_height * (getPF().bpp/8));
+      }
+    }
+  }
+  // Recreate the scaled pixel buffer
+  recreateScaledBuffer();
+  if (scaling && src_buffer && data) scaleRect(Rect(0, 0, src_width, src_height));
+  else if (!scaling && src_buffer) {
+    if (src_buffer->data && data) memcpy(data, src_buffer->data, src_buffer->area() * (getPF().bpp/8));
+    delete src_buffer;
+    src_buffer = 0;
+    src_data = 0;
+  }
+}
+
+void ScaledDIBSectionBuffer::calculateScaledBufferSize() {
+  ScaledPixelBuffer::calculateScaledBufferSize();
+  width_ = scaled_width;
+  height_ = scaled_height;
+}
+
+void ScaledDIBSectionBuffer::fillRect(const Rect &dest, Pixel pix) {
+  if (scaling) {
+    src_buffer->fillRect(dest, pix);
+    scaleRect(dest);
+  } else {
+    DIBSectionBuffer::fillRect(dest, pix);
+  }
+}
+
+void ScaledDIBSectionBuffer::imageRect(const Rect &dest, const void* pixels, int stride) {
+  if (scaling) {
+    src_buffer->imageRect(dest, pixels, stride);
+    scaleRect(dest);
+  } else {
+    DIBSectionBuffer::imageRect(dest, pixels, stride);
+  }
+}
+
+void ScaledDIBSectionBuffer::copyRect(const Rect &dest, const Point &move_by_delta) {
+  if (scaling) {
+    src_buffer->copyRect(dest, move_by_delta);
+    scaleRect(dest);
+  } else {
+    DIBSectionBuffer::copyRect(dest, move_by_delta);
+  }
+}
+      
+void ScaledDIBSectionBuffer::maskRect(const Rect& r, const void* pixels, const void* mask_) {
+  if (scaling) {
+    src_buffer->maskRect(r, pixels, mask_);
+    scaleRect(r);
+  } else {
+    DIBSectionBuffer::maskRect(r, pixels, mask_);
+  }
+}
+
+void ScaledDIBSectionBuffer::maskRect(const Rect& r, Pixel pixel, const void* mask_) {
+  if (scaling) {
+    src_buffer->maskRect(r, pixel, mask_);
+    scaleRect(r);
+  } else {
+    DIBSectionBuffer::maskRect(r, pixel, mask_);
+  }
+}

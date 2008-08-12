d48198d16148022b567dea28562bb50ad9e9ee89
syuri <syuri@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2008-08-12 10:08:18 +0000

    Heading comments have been added to a number of files.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@2658 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- orig/branches/1.3-win32-portable/vnc_winsrc/vncviewer/IniFileRegistry.h
+++ orig/branches/1.3-win32-portable/vnc_winsrc/vncviewer/IniFileRegistry.h
@@ -1,11 +1,29 @@
-#ifndef __VIRTUALREG_H__
-#define __VIRTUALREG_H__
+//  Copyright (C) 2008 GlavSoft LLC. All Rights Reserved.
+//
+//  This file is part of the TightVNC software.
+//
+//  TightVNC is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+//  USA.
+//
+// TightVNC homepage on the Web: http://www.tightvnc.com/
+
+#ifndef __INIFILEREGISTRY_H__
+#define __INIFILEREGISTRY_H__
 
 #include <map>
 #include "stdhdrs.h"
-
-#define PREF_STRING 'S'
-#define PREF_DWORD	'D'
 
 // Key of registr with one parameter
 struct ValueKey
@@ -43,4 +61,4 @@ public:
 	LSTATUS RegCloseKey(HKEY hKey);
 };
 
-#endif // __VIRTUALREG_H__
+#endif // __INIFILEREGISTRY_H__
--- orig/branches/1.3-win32-portable/vnc_winsrc/vncviewer/RegistryWrapper.h
+++ orig/branches/1.3-win32-portable/vnc_winsrc/vncviewer/RegistryWrapper.h
@@ -1,5 +1,26 @@
-#ifndef __SAVEOPTION_H__
-#define __SAVEOPTION_H__
+//  Copyright (C) 2008 GlavSoft LLC. All Rights Reserved.
+//
+//  This file is part of the TightVNC software.
+//
+//  TightVNC is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+//  USA.
+//
+// TightVNC homepage on the Web: http://www.tightvnc.com/
+
+#ifndef __REGISTRYWRAPPER_H__
+#define __REGISTRYWRAPPER_H__
 
 #include "TCHAR.h"
 #include "stdhdrs.h"
@@ -41,4 +62,4 @@ public:
 // Global link
 extern RegistryWrapper *registry;
 
-#endif // __SAVEOPTION_H__
+#endif // __REGISTRYWRAPPER_H__

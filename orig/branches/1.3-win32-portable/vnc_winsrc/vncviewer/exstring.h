b7844d7cd39e8193c2717c43085a5350251c22f6
syuri <syuri@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2008-08-18 04:46:50 +0000

    Functions _strCharToHexStr, _strHexStrToChar, _strXORString have been added.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@2668 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- orig/branches/1.3-win32-portable/vnc_winsrc/vncviewer/exstring.h
+++ orig/branches/1.3-win32-portable/vnc_winsrc/vncviewer/exstring.h
@@ -6,12 +6,15 @@
 
 extern inline void _tcsaddquotes(TCHAR *str);
 extern inline void _tcsremquotes(TCHAR *str);
-extern inline void _tcsextrword(TCHAR *str, TCHAR *extrWord);
+extern inline int _tcsextrword(TCHAR *str, TCHAR *extrWord);
 extern inline TCHAR *_tcsdelword(TCHAR *str, TCHAR *delsubstrword);
 
 extern inline void _straddquotes(char *str);
 extern inline void _strremquotes(char *str);
-extern inline void _strextrword(char *str, char *extrWord);
+extern inline int _strextrword(char *str, char *extrWord);
 extern inline char *_strdelword(char *str, char *delsub_strword);
+extern char *_strCharToHexStr(const char *mem, const int len);
+extern char *_strHexStrToChar(const char *hexStr, int *len);
+extern void _strXORString(char *str, const int lenStr, const char *key, const int lenKey);
 
 #endif // _EXSTRING_H_

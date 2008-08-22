f46cda25739eafa6bc3a7eef7685c0da502c3308
syuri <syuri@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2008-08-22 06:20:03 +0000

    Rectangle properties have been removed to public section.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@2700 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- orig/trunk/vnc_winsrc/libscreen/Rect.h
+++ orig/trunk/vnc_winsrc/libscreen/Rect.h
@@ -28,6 +28,11 @@ public:
   Rect(void) {};
   ~Rect(void) {};
   
+  int left;
+  int top;
+  int right;
+  int bottom;
+
   inline void SetRect(int lt, int tp, int rt, int bm)
   { 
     left = lt;
@@ -36,25 +41,12 @@ public:
     bottom = bm;
   }
 
-  inline void SetLeft(int value)    { left = value; }
-  inline void SetTop(int value)     { top = value; }
-  inline void SetRight(int value)   { right = value; }
-  inline void SetBottom(int value)  { bottom = value; }
   inline void SetWidth(int value)   { right = left + value; }
   inline void SetHeight(int value)  { bottom = top + value; }
 
-  inline int GetLeft()    { return left; }
-  inline int GetTop()     { return top; }
-  inline int GetRight()   { return right; }
-  inline int GetBottom()  { return bottom; }
-  inline int GetWidth()   { return right - left; }
-  inline int GetHeight()  { return bottom - top; }
+  inline int GetWidth()  const { return right - left; }
+  inline int GetHeight() const { return bottom - top; }
 
-private:
-  int left;
-  int top;
-  int right;
-  int bottom;
 };
 
 #endif // __RECT_H__

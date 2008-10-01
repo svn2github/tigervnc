9a2136c8727b142db8c63b0f3903f7ad5aaeb1e8
syuri <syuri@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2008-10-01 10:50:55 +0000

    [Development] Adding UpdateListener class for UpdateHandler event callback interface.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@2859 3789f03b-4d11-0410-bbf8-ca57d06f2519

---
+++ orig/trunk/vnc_winsrc/screen-updates/UpdateListener.h
@@ -0,0 +1,36 @@
+//  Copyright (C) 2008 GlavSoft LLC. All Rights Reserved.
+//
+//  This file is part of the TightVNC software.
+//
+//  TightVNC is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+//  USA.
+//
+// TightVNC homepage on the Web: http://www.tightvnc.com/
+
+#ifndef __UPDATELISTENER_H__
+#define __UPDATELISTENER_H__
+
+class UpdateListener
+{
+public:
+  UpdateListener(void);
+  virtual ~UpdateListener(void);
+
+protected:
+  // Interface function
+  virtual void onUpdate(void *pointer);
+};
+
+#endif // __UPDATELISTENER_H__

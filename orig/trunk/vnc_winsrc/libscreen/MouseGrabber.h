c3337631dffe4721fa57f3d600a57fc928080365
syuri <syuri@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2008-10-28 10:38:05 +0000

    [Development] Added new MouseGrabber and WindowsMouseGrabber classes.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@3066 3789f03b-4d11-0410-bbf8-ca57d06f2519

---
+++ orig/trunk/vnc_winsrc/libscreen/MouseGrabber.h
@@ -0,0 +1,43 @@
+//  Copyright (C) 2008 GlavSoft LLC. All Rights Reserved.
+//
+//  This file is part of the TightVNC software.
+//
+//  TightVNC is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+//  USA.
+//
+// TightVNC homepage on the Web: http://www.tightvnc.com/
+
+#ifndef __MOUSEGRABBER_H__
+#define __MOUSEGRABBER_H__
+
+#include "FrameBuffer.h"
+
+class MouseGrabber
+{
+public:
+  MouseGrabber(void);
+  virtual ~MouseGrabber(void);
+
+  virtual bool grab(PixelFormat *pixelFormat) = 0;
+
+  const FrameBuffer &getPixels() const { return m_pixels; }
+  const FrameBuffer &getMask() const { return m_mask; }
+
+protected:
+  FrameBuffer m_pixels;
+  FrameBuffer m_mask;
+};
+
+#endif // __MOUSEGRABBER_H__
---
+++ orig/trunk/vnc_winsrc/libscreen/WindowsMouseGrabber.h
@@ -0,0 +1,35 @@
+//  Copyright (C) 2008 GlavSoft LLC. All Rights Reserved.
+//
+//  This file is part of the TightVNC software.
+//
+//  TightVNC is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+//  USA.
+//
+// TightVNC homepage on the Web: http://www.tightvnc.com/
+
+#ifndef __WINDOWSMOUSEGRABBER_H__
+#define __WINDOWSMOUSEGRABBER_H__
+
+#include "MouseGrabber.h"
+#include "Windows.h"
+
+class WindowsMouseGrabber : public MouseGrabber
+{
+public:
+  WindowsMouseGrabber(void);
+  virtual ~WindowsMouseGrabber(void);
+};
+
+#endif // __WINDOWSMOUSEGRABBER_H__

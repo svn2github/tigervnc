svn2github/tigervnc

commit 9db5142e9a429e4ccc2b47b50a5668db436d8d8e

[Bugfixes, cleanups] Fixed the logic of vncClient::SendUpdate(). Now we do not clear the whole "changed region" on sending some data, we subtract the actually sent region from the changed region instead. Also, we should process both full and incremental updates correctly instead of discarding incremental updates on full update requests.
git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@3120 3789f03b-4d11-0410-bbf8-ca57d06f2519

orig/trunk/vnc_winsrc/winvnc/vncClient.cpp
@@ -2236,46 +2236,47 @@ vncClient::SendUpdate()
 	normalUpdates.Clear();
 
 	if (!m_full_rgn.IsEmpty()) {
-		m_incr_rgn.Clear(); // FIXME: Why incremental region is cleared here?
 		m_copyrect_set = false; // FIXME: Why CopyRect is removed even if it is not in m_full_region?
 		normalUpdates.Combine(m_full_rgn);
-		m_changed_rgn.Clear(); // FIXME: Should not we remove changes only within m_full_region?
 		m_full_rgn.Clear();
-	} else {
-		if (!m_incr_rgn.IsEmpty()) {
-			// Get region to send from WinDesktop
-			normalUpdates.Combine(m_changed_rgn);
-			// FIXME: Should we intersect normalUpdates with shared desktop area?
-			// FIXME: Should we intersect normalUpdates with m_incr_rgn?
-
-			// Mouse stuff for the case when cursor shape updates are off
-			if (!m_cursor_update_sent && !m_cursor_update_pending) {
-				// If the mouse hasn't moved, see if its position is in the rect
-				// we're sending. If so, make sure the full mouse rect is sent.
-				if (!m_mousemoved) {
-					vncRegion tmpMouseRgn;
-					tmpMouseRgn.AddRect(m_oldmousepos);
-					tmpMouseRgn.Intersect(normalUpdates);
-					if (!tmpMouseRgn.IsEmpty()) 
-						m_mousemoved = TRUE;
-				}
-				// If the mouse has moved (or otherwise needs an update):
-				if (m_mousemoved) {
-					// Include an update for its previous position
-					if (IntersectRect(&m_oldmousepos, &m_oldmousepos, &m_server->GetSharedRect())) 
-						normalUpdates.AddRect(m_oldmousepos);
-					// Update the cached mouse position
-					m_oldmousepos = m_buffer->GrabMouse();
-					// Include an update for its current position
-					if (IntersectRect(&m_oldmousepos, &m_oldmousepos, &m_server->GetSharedRect())) 
-						normalUpdates.AddRect(m_oldmousepos);
-					// Indicate the move has been handled
-					m_mousemoved = FALSE;
-				}
-			}
-			m_changed_rgn.Clear(); // FIXME: Clear just what we actually sent?
-		}
 	}
+
+	if (!m_incr_rgn.IsEmpty()) {
+		// Get region to send from WinDesktop
+		normalUpdates.Combine(m_changed_rgn);
+		// FIXME: Should we intersect normalUpdates with shared desktop area?
+		// FIXME: Should we intersect normalUpdates with m_incr_rgn?
+
+		// Mouse stuff for the case when cursor shape updates are off
+		if (!m_cursor_update_sent && !m_cursor_update_pending) {
+			// If the mouse hasn't moved, see if its position is in the rect
+			// we're sending. If so, make sure the full mouse rect is sent.
+			if (!m_mousemoved) {
+				vncRegion tmpMouseRgn;
+				tmpMouseRgn.AddRect(m_oldmousepos);
+				tmpMouseRgn.Intersect(normalUpdates);
+				if (!tmpMouseRgn.IsEmpty()) 
+					m_mousemoved = TRUE;
+			}
+			// If the mouse has moved (or otherwise needs an update):
+			if (m_mousemoved) {
+				// Include an update for its previous position
+				if (IntersectRect(&m_oldmousepos, &m_oldmousepos, &m_server->GetSharedRect())) 
+					normalUpdates.AddRect(m_oldmousepos);
+				// Update the cached mouse position
+				m_oldmousepos = m_buffer->GrabMouse();
+				// Include an update for its current position
+				if (IntersectRect(&m_oldmousepos, &m_oldmousepos, &m_server->GetSharedRect())) 
+					normalUpdates.AddRect(m_oldmousepos);
+				// Indicate the move has been handled
+				m_mousemoved = FALSE;
+			}
+		}
+		m_incr_rgn.Clear();
+	}
+
+    // The region that will be sent should be excluded from m_changed_rgn.
+    m_changed_rgn.Subtract(normalUpdates);
 
     // Get the final list of normal rectangles to send.
     rectlist normalUpdatesList;
@@ -2298,9 +2299,7 @@ vncClient::SendUpdate()
 		if (m_copyrect_set)
 			numrects++;
 		// If there are no rectangles then return
-		if (numrects != 0)
-			m_incr_rgn.Clear();
-		else
+		if (numrects == 0)
 			return FALSE;
 	}
 

commit 91b305cbff1c562bec3652077f631a956297b0e7

[Bugfix] Fixed a bug introduced in r3120 - vncClient::SendUpdate() used to clear m_incr_rgn even if it did not actually send updates.
git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@3124 3789f03b-4d11-0410-bbf8-ca57d06f2519

orig/trunk/vnc_winsrc/winvnc/vncClient.cpp
@@ -2272,7 +2272,6 @@ vncClient::SendUpdate()
 				m_mousemoved = FALSE;
 			}
 		}
-		m_incr_rgn.Clear();
 	}
 
     // The region that will be sent should be excluded from m_changed_rgn.
@@ -2303,9 +2302,12 @@ vncClient::SendUpdate()
 			return FALSE;
 	}
 
+    // Now we know that something will be sent so we may clear m_incr_rgn.
+    m_incr_rgn.Clear();
+
 	omni_mutex_lock l(m_sendUpdateLock);
 
-	// Otherwise, send <number of rectangles> header
+	// Send <number of rectangles> header.
 	rfbFramebufferUpdateMsg header;
     if (numrects == -1) {
       header.nRects = Swap16IfLE(0xFFFF);
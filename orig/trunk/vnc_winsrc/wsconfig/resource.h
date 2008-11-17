9dd07174c09d33ef133ca6412a184c8779fcf6b9
const_k <const_k@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2008-11-17 20:58:07 +0000

    [User interface] Designed dialogs for the upcoming standalone configurator of the TightVNC Server. Currently, it is intended to configure mappings of TCP ports to screen areas.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@3176 3789f03b-4d11-0410-bbf8-ca57d06f2519

---
+++ orig/trunk/vnc_winsrc/wsconfig/resource.h
@@ -0,0 +1,12 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDC_PORT_EDIT                           1002
+#define IDC_APPLY                               1003
+#define IDC_TAB                                 1005
+#define IDC_GEOMETRY_EDIT                       1009
+#define IDC_ADD_PORT                            1017
+#define IDC_EDIT_PORT                           1018
+#define IDC_REMOVE_PORT                         1019
+#define IDC_MAPPINGS                            1020

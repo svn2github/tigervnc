716180f3f0bb85f7c625e600054c6b370dd0280b
enikey <enikey@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2008-11-19 11:37:01 +0000

    [Layout] Added solution "wsconfig", project "wsconfig", auto-generated VS header and source files.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@3185 3789f03b-4d11-0410-bbf8-ca57d06f2519

---
+++ orig/trunk/vnc_winsrc/wsconfig/wsconfig.cpp
@@ -0,0 +1,11 @@
+// wsconfig.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "wsconfig.h"
+
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                       LPTSTR lpCmdLine, int nCmdShow)
+{
+  return 0;
+}
---
+++ orig/trunk/vnc_winsrc/wsconfig/wsconfig.h
@@ -0,0 +1,6 @@
+#ifndef _WSCONFIG_H_
+#define _WSCONFIG_H_
+
+#include "resource.h"
+
+#endif

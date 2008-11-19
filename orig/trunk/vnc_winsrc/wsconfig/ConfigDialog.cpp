6ec75db7f6ca79704f182aa8768ccb31cc981f11
enikey <enikey@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2008-11-19 12:17:48 +0000

    [Developement] Added empty dialog events handlers (onOKButtonClick, onApplyButtonClick, etc). Added members m_ctrlThis, m_ctrlMappingsListBox.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@3193 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- orig/trunk/vnc_winsrc/wsconfig/ConfigDialog.cpp
+++ orig/trunk/vnc_winsrc/wsconfig/ConfigDialog.cpp
@@ -8,3 +8,31 @@ ConfigDialog::ConfigDialog(void)
 ConfigDialog::~ConfigDialog(void)
 {
 }
+
+void ConfigDialog::onInitDialog(HWND hWnd)
+{
+}
+
+void ConfigDialog::onAddButtonClick()
+{
+}
+
+void ConfigDialog::onEditButtonClick()
+{
+}
+
+void ConfigDialog::onRemoveButtonClick()
+{
+}
+
+void ConfigDialog::onCancelButtonClick()
+{
+}
+
+void ConfigDialog::onOKButtonClick()
+{
+}
+
+void ConfigDialog::onApplyButtonClick()
+{
+}
--- orig/trunk/vnc_winsrc/wsconfig/ConfigDialog.h
+++ orig/trunk/vnc_winsrc/wsconfig/ConfigDialog.h
@@ -1,11 +1,28 @@
 #ifndef _CONFIG_DIALOG_H_
 #define _CONFIG_DIALOG_H_
 
+#include "ui/Control.h"
+#include "ui/ListBox.h"
+
 class ConfigDialog
 {
 public:
   ConfigDialog(void);
   ~ConfigDialog(void);
+protected:
+  // Init dialog handler
+  void onInitDialog(HWND hWnd);
+  // Button handlers
+  void onAddButtonClick();
+  void onEditButtonClick();
+  void onRemoveButtonClick();
+  void onCancelButtonClick();
+  void onOKButtonClick();
+  void onApplyButtonClick();
+protected:
+  // Controls
+  Control m_ctrlThis;
+  ListBox m_ctrlMappingListBox;
 };
 
 #endif

8069b31c07e292e5f3709cb2b1d94407989e35fa
enikey <enikey@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2008-11-24 09:18:16 +0000

    [Developement] Added class Rect.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@3220 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- orig/trunk/vnc_winsrc/wsconfig/Rect.cpp
+++ orig/trunk/vnc_winsrc/wsconfig/Rect.cpp
@@ -1,2 +1,11 @@
 #include "StdAfx.h"
 #include "Rect.h"
+
+Rect::Rect()
+: x(0), y(0), width(0), height(0)
+{
+}
+
+Rect::~Rect()
+{
+}
--- orig/trunk/vnc_winsrc/wsconfig/Rect.h
+++ orig/trunk/vnc_winsrc/wsconfig/Rect.h
@@ -1,4 +1,16 @@
 #ifndef _RECT_H_
 #define _RECT_H_
 
+class Rect
+{
+public:
+  Rect();
+  ~Rect();
+public:
+  int x;
+  int y;
+  int width;
+  int height;
+};
+
 #endif

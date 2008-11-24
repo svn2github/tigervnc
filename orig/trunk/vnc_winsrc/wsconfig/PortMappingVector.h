8b256d6a6f0d1397406376302469ff24385c10a9
enikey <enikey@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2008-11-24 09:28:05 +0000

    [Layout, Developement] Added class PortMappingVector.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@3222 3789f03b-4d11-0410-bbf8-ca57d06f2519

---
+++ orig/trunk/vnc_winsrc/wsconfig/PortMappingVector.h
@@ -0,0 +1,11 @@
+#ifndef _PORT_MAPPING_VECTOR_H_
+#define _PORT_MAPPING_VECTOR_H_
+
+#include <vector>
+#include "PortMapping.h"
+
+using namespace std;
+
+typedef vector<PortMapping> PortMappingVector;
+
+#endif

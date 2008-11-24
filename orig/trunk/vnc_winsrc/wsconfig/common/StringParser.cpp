83d99ec339857ebd9dd67e43384d67266972592e
enikey <enikey@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2008-11-24 10:14:44 +0000

    [Developement] Added method tryParseInt.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@3229 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- orig/trunk/vnc_winsrc/wsconfig/common/StringParser.cpp
+++ orig/trunk/vnc_winsrc/wsconfig/common/StringParser.cpp
@@ -1,2 +1,14 @@
 #include "StdAfx.h"
 #include "StringParser.h"
+#include <tchar.h>
+
+bool StringParser::tryParseInt(tstring str)
+{
+  TCHAR* c_str = (TCHAR *)str.c_str();
+  int value = 0;
+  if(_stscanf(c_str, _T("%d"), &value)  == EOF) {
+    /* error */
+    return false;
+  }
+  return true;
+}
--- orig/trunk/vnc_winsrc/wsconfig/common/StringParser.h
+++ orig/trunk/vnc_winsrc/wsconfig/common/StringParser.h
@@ -1,4 +1,12 @@
 #ifndef _STRING_PARSER_H_
 #define _STRING_PARSER_H_
 
+#include "tstring.h"
+
+class StringParser
+{
+public:
+  static bool tryParseInt(tstring str);
+};
+
 #endif

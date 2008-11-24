944748a5e8a4c99db4dffbac74681763224ee46e
enikey <enikey@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2008-11-24 07:48:58 +0000

    [Developement] Added base methods to class BaseDialog.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@3209 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- orig/trunk/vnc_winsrc/wsconfig/ui/BaseDialog.cpp
+++ orig/trunk/vnc_winsrc/wsconfig/ui/BaseDialog.cpp
@@ -3,8 +3,75 @@
 
 BaseDialog::BaseDialog()
 {
+  m_resourceName = _T("");
+}
+
+BaseDialog::BaseDialog(tstring resourceName)
+{
+  m_resourceName = resourceName;
+}
+
+BaseDialog::BaseDialog(LPTSTR resourceName)
+{
+  m_resourceName = resourceName;
 }
 
 BaseDialog::~BaseDialog()
 {
+}
+
+void BaseDialog::setResourceName(LPTSTR resourceName)
+{
+  m_resourceName = resourceName;
+}
+
+int BaseDialog::show()
+{
+  return 0;
+}
+
+int BaseDialog::showModal()
+{
+  return 0;
+}
+
+void BaseDialog::onInitDialog()
+{
+}
+
+void BaseDialog::onNotify(UINT controlID, LPARAM data)
+{
+}
+
+void BaseDialog::onCommand(UINT controlID, UINT notificationID)
+{
+}
+
+void BaseDialog::onDestroy()
+{
+}
+
+BOOL CALLBACK BaseDialog::modalDialogProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{  
+  BaseDialog *_this = (BaseDialog *)GetWindowLong(hwnd, GWL_USERDATA);
+
+  switch (uMsg) {
+  case WM_INITDIALOG:
+    _this = (BaseDialog *)lParam;
+    SetWindowLong(hwnd, GWL_USERDATA, (LONG)_this);
+    _this->m_ctrlThis.setWindow(hwnd);
+    _this->onInitDialog();
+    break;
+  case WM_NOTIFY:
+    _this->onNotify(LOWORD(wParam), lParam);
+    break;
+  case WM_COMMAND:
+    _this->onCommand(LOWORD(wParam), HIWORD(wParam));
+    break;
+  case WM_DESTROY:
+    _this->onDestroy();
+    break;
+  }
+
+  return FALSE;
 }
--- orig/trunk/vnc_winsrc/wsconfig/ui/BaseDialog.h
+++ orig/trunk/vnc_winsrc/wsconfig/ui/BaseDialog.h
@@ -1,11 +1,29 @@
 #ifndef _BASE_DIALOG_H_
 #define _BASE_DIALOG_H_
 
+#include "common/tstring.h"
+#include "Control.h"
+
 class BaseDialog
 {
 public:
   BaseDialog();
+  BaseDialog(tstring resourceName);
+  BaseDialog(LPTSTR resourceName);
   ~BaseDialog();
+public:
+  int show();
+  int showModal();
+  void setResourceName(LPTSTR resourceName);
+protected:
+  virtual void onInitDialog();
+  virtual void onNotify(UINT controlID, LPARAM data);
+  virtual void onCommand(UINT controlID, UINT notificationID);
+  virtual void onDestroy();
+  static BOOL CALLBACK modalDialogProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+protected:
+  tstring m_resourceName; // Name of dialog resource
+  Control m_ctrlThis;     // This dialog control
 };
 
 #endif

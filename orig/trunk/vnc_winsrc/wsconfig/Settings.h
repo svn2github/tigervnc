3905c343b326021f9575b7b8eb7acdb1f87370a9
enikey <enikey@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2008-11-24 09:31:59 +0000

    [Layout, Developement] Added class Settings.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@3223 3789f03b-4d11-0410-bbf8-ca57d06f2519

---
+++ orig/trunk/vnc_winsrc/wsconfig/Settings.h
@@ -0,0 +1,14 @@
+#ifndef _SETTINGS_H_
+#define _SETTINGS_H_
+
+#include "PortMappingVector.h"
+
+class Settings
+{
+public:
+  Settings();
+public:
+  PortMappingVector m_vPortMapping;
+};
+
+#endif

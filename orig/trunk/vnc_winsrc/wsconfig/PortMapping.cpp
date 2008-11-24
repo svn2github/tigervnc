2c66fa2ab030c5051d186384514fa14410e4e5f9
enikey <enikey@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2008-11-24 09:21:03 +0000

    [Layout, Developement] Added class PortMapping.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@3221 3789f03b-4d11-0410-bbf8-ca57d06f2519

---
+++ orig/trunk/vnc_winsrc/wsconfig/PortMapping.cpp
@@ -0,0 +1,7 @@
+#include "StdAfx.h"
+#include "PortMapping.h"
+
+PortMapping::PortMapping()
+: port(0)
+{
+}
---
+++ orig/trunk/vnc_winsrc/wsconfig/PortMapping.h
@@ -0,0 +1,15 @@
+#ifndef _PORT_MAPPING_H_
+#define _PORT_MAPPING_H_
+
+#include "Rect.h"
+
+class PortMapping
+{
+public:
+  PortMapping();
+public:
+  int port;
+  Rect rect;
+};
+
+#endif

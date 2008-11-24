88645906ac4889108ad8af790401a425402ba760
enikey <enikey@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2008-11-24 10:44:53 +0000

    [Developement] Added getRect, setRect methods, m_rect member. Added isUserDataValid method.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@3233 3789f03b-4d11-0410-bbf8-ca57d06f2519

--- orig/trunk/vnc_winsrc/wsconfig/PortMappingDialog.cpp
+++ orig/trunk/vnc_winsrc/wsconfig/PortMappingDialog.cpp
@@ -1,5 +1,6 @@
 #include "StdAfx.h"
 #include "Resource.h"
+#include "common/StringParser.h"
 #include "PortMappingDialog.h"
 
 PortMappingDialog::PortMappingDialog()
@@ -25,6 +26,22 @@ void PortMappingDialog::initControls()
 {
   HWND dialogHwnd = m_ctrlThis.getWindow();
   m_geometryTextBox.setWindow(GetDlgItem(dialogHwnd, IDC_GEOMETRY_EDIT));
+  m_portTextBox.setWindow(GetDlgItem(dialogHwnd, IDC_PORT_EDIT));
+}
+
+bool PortMappingDialog::isUserDataValid()
+{
+  if (!Rect::tryParse((TCHAR *)m_geometryTextBox.getText().c_str())) {
+    MessageBox(m_ctrlThis.getWindow(), _T("Wrong geometry string format"), _T("Error"), MB_OK | MB_ICONWARNING);
+    return false;
+  }
+  int port;
+  StringParser::parseInt((TCHAR *)m_portTextBox.getText().c_str(), &port);
+  if ((port < 1) || (port > 65535)) {
+    MessageBox(m_ctrlThis.getWindow(), _T("Port must be between 1 and 65535"), _T("Error"), MB_OK | MB_ICONWARNING);
+    return false;
+  }
+  return true;
 }
 
 void PortMappingDialog::onInitDialog()
--- orig/trunk/vnc_winsrc/wsconfig/PortMappingDialog.h
+++ orig/trunk/vnc_winsrc/wsconfig/PortMappingDialog.h
@@ -3,6 +3,7 @@
 
 #include "ui/BaseDialog.h"
 #include "ui/TextBox.h"
+#include "Rect.h"
 
 class PortMappingDialog : public BaseDialog
 {
@@ -11,12 +12,18 @@ public:
   ~PortMappingDialog();
 protected:
   void initControls();
+  bool isUserDataValid();
   virtual void onInitDialog();
   virtual void onCommand(UINT cID, UINT nID);
   void onOkButtonClick();
   void onCancelButtonClick();
+
+  Rect getRect() { return m_rect; }
+  void setRect(Rect rect) { m_rect = rect; }
 protected:
   TextBox m_geometryTextBox;
+  TextBox m_portTextBox;
+  Rect m_rect;
 };
 
 #endif

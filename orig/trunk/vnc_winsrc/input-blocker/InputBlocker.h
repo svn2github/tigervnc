6d3ba05c08da7051bba50aebc6fe4faa11a13ecd
syuri <syuri@3789f03b-4d11-0410-bbf8-ca57d06f2519>
2008-12-04 10:59:22 +0000

    [Development] Adding new input-blocker project with a number of function.
    git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@3299 3789f03b-4d11-0410-bbf8-ca57d06f2519

---
+++ orig/trunk/vnc_winsrc/input-blocker/InputBlocker.h
@@ -0,0 +1,64 @@
+//  Copyright (C) 2008 GlavSoft LLC. All Rights Reserved.
+//
+//  This file is part of the TightVNC software.
+//
+//  TightVNC is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+//  USA.
+//
+// TightVNC homepage on the Web: http://www.tightvnc.com/
+
+#ifndef __INPUTBLOCKER_H__
+#define __INPUTBLOCKER_H__
+
+#include <windows.h>
+#include "thread/Thread.h"
+
+// Only one instance of this class may be created.
+
+class InputBlocker : protected Thread
+{
+public:
+  InputBlocker(void);
+  virtual ~InputBlocker(void);
+
+  bool setKeyboardBlocking(bool block);
+  bool setMouseBlocking(bool block);
+
+protected:
+  virtual void execute();
+  virtual void onTerminate();
+
+  bool setKeyboardFilterHook(bool block);
+  bool setMouseFilterHook(bool block);
+
+  static LRESULT CALLBACK lowLevelKeyboardFilterProc(int nCode,
+                                                     WPARAM wParam,
+                                                     LPARAM lParam);
+  static LRESULT CALLBACK lowLevelMouseFilterProc(int nCode,
+                                                  WPARAM wParam,
+                                                  LPARAM lParam);
+
+  static HHOOK m_hKeyboardHook;
+  static HHOOK m_hMouseHook;
+
+  static bool m_isSecondaryInstance;
+
+  bool m_isValid;
+
+  bool m_isKeyboardBlocking;
+  bool m_isMouseBlocking;
+};
+
+#endif // __INPUTBLOCKER_H__

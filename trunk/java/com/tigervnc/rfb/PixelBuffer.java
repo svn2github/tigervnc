svn2github/tigervnc

commit 0a7265660fcb85a1243d88a7a9b46ba3722b9fe4

fixed bug where IndexColorModel was reset when switching to 8bpp from another depth
git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@5016 3789f03b-4d11-0410-bbf8-ca57d06f2519

trunk/java/com/tigervnc/rfb/PixelBuffer.java
@@ -41,13 +41,12 @@ public class PixelBuffer {
     case  6: 
       // Fall-through to depth 8
     case  8: 
+      if (cm != null && cm instanceof IndexColorModel)
+        break;
       int rmask = pf.redMax << pf.redShift;
       int gmask = pf.greenMax << pf.greenShift;
       int bmask = pf.blueMax << pf.blueShift;
-      if (pf.trueColour)
-        cm = new DirectColorModel(8, rmask, gmask, bmask);
-      else
-        cm = new IndexColorModel(8, 256, new byte[256], new byte[256], new byte[256]);
+      cm = new DirectColorModel(8, rmask, gmask, bmask);
       break;
     case 16: 
       cm = new DirectColorModel(32, 0xF800, 0x07C0, 0x003E, (0xff << 24));

trunk/java/com/tigervnc/vncviewer/BIPixelBuffer.java
@@ -38,7 +38,7 @@ public class BIPixelBuffer extends PlatformPixelBuffer implements ImageObserver
   }
 
   public void updateColourMap() {
-    cm = new IndexColorModel(8, nColours, reds, greens, blues);
+    super.updateColourMap();
     createImage(width_, height_);
   }
   

commit 3b63b094acd4e69e9ee9979ee4dbfb66ee52423a

Fixes regression introduced by r5016 (exception produced when connecting to 8bpp server)
git-svn-id: https://tigervnc.svn.sourceforge.net/svnroot/tigervnc@5017 3789f03b-4d11-0410-bbf8-ca57d06f2519

trunk/java/com/tigervnc/rfb/PixelBuffer.java
@@ -41,8 +41,11 @@ public class PixelBuffer {
     case  6: 
       // Fall-through to depth 8
     case  8: 
-      if (cm != null && cm instanceof IndexColorModel)
+      if (!pf.trueColour) {
+        if (cm == null)
+          cm = new IndexColorModel(8, 256, new byte[256], new byte[256], new byte[256]);
         break;
+      }
       int rmask = pf.redMax << pf.redShift;
       int gmask = pf.greenMax << pf.greenShift;
       int bmask = pf.blueMax << pf.blueShift;